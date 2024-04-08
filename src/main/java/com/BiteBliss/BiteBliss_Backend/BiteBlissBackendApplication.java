81ad01239501cc6b9ed8ce6ed030b28d491282fd
Li Haoyuan <1513624626@qq.com>
2024-04-08 23:38:53 +0800

    项目初始化

---
+++ src/main/java/com/BiteBliss/BiteBliss_Backend/BiteBlissBackendApplication.java
@@ -0,0 +1,13 @@
+package com.BiteBliss.BiteBliss_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteBlissBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteBlissBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BiteBliss/BiteBliss_Backend/BiteBlissBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BiteBliss.BiteBliss_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteBlissBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
